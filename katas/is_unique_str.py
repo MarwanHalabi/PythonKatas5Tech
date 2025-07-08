0104de40add1d429f0319ae8db06f8e8f48d30fa
MarwanHalabi <m.halvi@hotmail.com>
2025-07-08 12:49:18 +0300

    is unique

--- katas/is_unique_str.py
+++ katas/is_unique_str.py
@@ -8,7 +8,7 @@ def is_unique(string):
     Returns:
         True if all characters are unique, False otherwise
     """
-    return True
+    return True if len(string) == len(set(string)) else False
 
 
 if __name__ == '__main__':
